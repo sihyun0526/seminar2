27a3019aa6c30d9295b7a06317aa5c8cad16a5ae
김시현 <kimsihyun@gimsihyeon-ui-MacBookPro.local>
2024-07-31 21:29:36 +0900

    수정

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from github_utils import get_github_repo, upload_github_issue
 
 
 if __name__ == "__main__":
-    access_token = os.environ['MY_GITHUB_TOKEN']
     repository_name = "github-action-with-python"
 
     seoul_timezone = timezone('Asia/Seoul')
